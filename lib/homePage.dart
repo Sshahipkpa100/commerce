0707d3994c0223553f90d09ca70c736d340e3cf9
Aashutosh <shahipkpa18@gmail.com>
2022-07-21 10:13:31 +0545

    minor

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
       ),
         body: Center(
           child: Container(
-            child: Text("Welcome to 30 days of flutter"),
+            child: Text("Welcome to 1st days of flutter"),
           ),
         ),
         drawer: Drawer(),
